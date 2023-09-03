2371d094fcc412a5dd10f1ebf1608e293d5852c8
Lukaszc97 <114765173+Lukaszc97@users.noreply.github.com>
2023-09-03 17:36:55 +0200

    d

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 const Statistics = ({ good, neutral, bad }) => {
   const total = good + neutral + bad;
-  const average = (good - bad) / total || 0;
+
   const positivePercentage = (good / total) * 100 || 0;
 
   return (
